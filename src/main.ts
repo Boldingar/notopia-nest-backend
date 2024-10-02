8ab94018354b079c22ed6fb948643c2b0d8b23c9
haneen akram <nonakram@gmail.com>
2024-10-03 01:31:24 +0200

    updated the img upload

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Serve static files from the 'images/categories' folder
+  app.use(
+    '/images/categories',
+    express.static(join(__dirname, '..', 'src', 'images', 'categories')),
+  );
 
   app.enableCors();
 
